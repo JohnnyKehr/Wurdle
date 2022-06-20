30cedeb85cea6ee3e1998ecfbdd101b7e247e865
Johnny Kehr <jokehr@cisco.com>
2022-06-20 10:03:56 -0700

    Initial workspace

---
+++ Wurdle/Wurdle/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Wurdle
+//
+//  Created by jokehr on 6/20/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Wurdle/Wurdle/WurdleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WurdleApp.swift
+//  Wurdle
+//
+//  Created by jokehr on 6/20/22.
+//
+
+import SwiftUI
+
+@main
+struct WurdleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
